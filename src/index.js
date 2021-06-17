32a4c47cb0fef172e948f6d94fafd2f70f7b6f45
Nikolay Mosalov <73695807+Rom-dev-tech@users.noreply.github.com>
2021-06-17 22:44:10 +0300

    updates

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './sass/main.scss';
 import CountdownTimer from './js/сountdownTimer';
 
-
 const timer = new CountdownTimer({
   selector: '#timer-1',
   targetDate: new Date('jan 01, 2022'),
--- src/js/сountdownTimer.js
+++ src/js/сountdownTimer.js
@@ -1,50 +1,75 @@
-export default function Timer({ selector, targetDate }) {
-  let id = 0;
+export default class Timer {
+  constructor({ selector, targetDate }) {
+    this.intervalId = null;
+    this.isActive = false;
+    this.targetDate = targetDate;
+
+    // Рефы
+    this.daysValue = document.querySelector(`${selector} [data-value="days"]`);
+    this.hoursValue = document.querySelector(`${selector} [data-value="hours"]`);
+    this.minsValue = document.querySelector(`${selector} [data-value="mins"]`);
+    this.secsValue = document.querySelector(`${selector} [data-value="secs"]`);
+
+    this.daysText = document.querySelector(`${selector} .time-count__days .time-count__text`);
+    this.hoursText = document.querySelector(`${selector} .time-count__hours .time-count__text`);
+    this.minutesText = document.querySelector(`${selector} .time-count__minutes .time-count__text`);
+    this.secondsText = document.querySelector(`${selector} .time-count__seconds .time-count__text`);
+  }
+
+  start = () => {
+    if (this.isActive) {
+      return;
+    }
+
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const diff = this.targetDate - Date.now();
+      const time = this.getTimeComponents(diff);
+      this.runClockFace(time);
+
+      // * Если событее прошло
+      if (diff < 0) {
+        clearInterval(this.intervalId);
+        this.isActive = false;
+        document.querySelector(selector).innerHTML = `<p class="end">EXPIRED</p>`;
+      }
+    }, 1000);
+  };
+
+  // Функция обработки на дни часы минуты и секунды
+  getTimeComponents(time) {
+    const days = this.addZero(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.addZero(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = this.addZero(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const seconds = this.addZero(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, seconds };
+  }
 
   //* Add Zeros
-function addZero(n) {
-  return (parseInt(n, 10) < 10 ? '0' : '') + n;
+  addZero(n) {
+    return (parseInt(n, 10) < 10 ? '0' : '') + n;
   }
 
-  function declOfNum(number, titles) {  
-		let cases = [2, 0, 1, 1, 1, 2];  
-		return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-	}
-  
-  const daysValue = document.querySelector(`${selector} [data-value="days"]`);
-  const hoursValue = document.querySelector(`${selector} [data-value="hours"]`);
-  const minsValue = document.querySelector(`${selector} [data-value="mins"]`);
-  const secsValue = document.querySelector(`${selector} [data-value="secs"]`);
-
-  const daysText = document.querySelector(`${selector} .time-count__days .time-count__text`);
-	const hoursText = document.querySelector(`${selector} .time-count__hours .time-count__text`);
-	const minutesText = document.querySelector(`${selector} .time-count__minutes .time-count__text`);
-	const secondsText = document.querySelector(`${selector} .time-count__seconds .time-count__text`);
-
-  this.start = () => {
-        
-        id = setInterval(() => {
-          const diff = targetDate - Date.now();
-          const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-          const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-          const mins = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-          const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-            
-          daysValue.textContent = addZero(days);
-          hoursValue.textContent = addZero(hours);
-          minsValue.textContent = addZero(mins);
-          secsValue.textContent = addZero(seconds);
-
-          daysText.textContent = declOfNum(days, ['день', 'дня', 'дней']);
-		      hoursText.textContent = declOfNum(hours, ['час', 'часа', 'часов']);
-		      minutesText.textContent = declOfNum(mins, ['минута', 'минуты', 'минут']);
-		      secondsText.textContent = declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
-          // * Если событее прошло
-          if (diff < 0) {
-            clearInterval(id);
-              id = 0;
-            document.querySelector(selector).innerHTML = `<p class="end">EXPIRED</p>`;
+  // Функция для лайблов
+  declOfNum(number, titles) {
+    let cases = [2, 0, 1, 1, 1, 2];
+    return titles[
+      number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]
+    ];
   }
-        }, 100);
-    };
+
+  // Отресовка
+  runClockFace({ days, hours, mins, seconds }) {
+    this.daysValue.textContent = days;
+    this.hoursValue.textContent = hours;
+    this.minsValue.textContent = mins;
+    this.secsValue.textContent = seconds;
+
+    this.daysText.textContent = this.declOfNum(days, ['день', 'дня', 'дней']);
+    this.hoursText.textContent = this.declOfNum(hours, ['час', 'часа', 'часов']);
+    this.minutesText.textContent = this.declOfNum(mins, ['минута', 'минуты', 'минут']);
+    this.secondsText.textContent = this.declOfNum(seconds, ['секунда', 'секунды', 'секунд']);
+  }
 }
